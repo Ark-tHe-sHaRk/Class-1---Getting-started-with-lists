d4eb20cdab310def52088d2b73306c13c46f3334
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2024-12-09 17:40:14 +0000

    Lists

---
+++ List.py
@@ -0,0 +1,13 @@
+#Creating a Empty List:
+Empty_list = []
+print("Printing an empty list.", Empty_list)
+#Creating a List with elements:
+List_1 = [1, 2, 3, 4, "Ark"]
+print("Printing list with elements", List_1)
+#Use a multiplication operator
+List_2 = [1, 2, 2, 2, 2]*3
+print("Printing list with multiplication", List_2)
+#Reversing the given list
+List_3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+List_3 = List_3[::-1]
+print("Reversing the given list", List_3)
